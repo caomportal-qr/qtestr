5973932cbb555c34811bc69820fc8451f735c7b4
caomportal-qr <34678281+caomportal-qr@users.noreply.github.com>
2018-01-19 18:29:11 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -31,6 +31,18 @@ function main() {
         var select = $("<select>");
         select.change(select_change);
 
+//2018.01.19 test start
+var ilength=orig_cameras.length;
+var message="orig_cameras.length";
+var icamera=default_camera_id;
+var message2="default_camera_idh";
+ alert(message+ilength);
+ alert(message2+icamera);
+           if(orig_cameras.length ==1 && default_camera_id != 0 ) {
+                default_camera_id = 0;
+            }
+//2018.01.19 test end
+
         for(var i = 0; i < orig_cameras.length; i++) {
             var camera = orig_cameras[i];
             cameras[camera.id] = camera;
@@ -47,17 +59,6 @@ function main() {
                 default_camera_id = camera.id;
             }
 
-//2018.01.19 test start
-var ilength=orig_cameras.length;
-var message="orig_cameras.length";
-var icamera=default_camera_id;
-var message2="default_camera_idh";
- alert(message+ilength);
- alert(message2+icamera);
-           if(orig_cameras.length ==1 && default_camera_id != 0 ) {
-                default_camera_id = 0;
-            }
-//2018.01.19 test end
 
             askreload = askreload || camera.name == null;
         }
