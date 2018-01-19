2d2a525264e61e76730bceb4a622af60b4b7a1f8
caomportal-qr <34678281+caomportal-qr@users.noreply.github.com>
2018-01-19 18:11:21 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -48,7 +48,13 @@ function main() {
             }
 
 //2018.01.19 test start
-            if(orig_cameras.length ==1 && default_camera_id != 0 ) {
+var ilength=orig_cameras.length;
+var message="orig_cameras.length";
+var icamera=default_camera_id;
+var message2="default_camera_idh";
+ alert(message+ilength);
+ alert(message2+icamera);
+           if(orig_cameras.length ==1 && default_camera_id != 0 ) {
                 default_camera_id = 0;
             }
 //2018.01.19 test end
